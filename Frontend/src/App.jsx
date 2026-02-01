f6271a2a24f32aed1ecf226ce95fa74f224a40b8
mphart <mhart5892@gmail.com>
2026-01-31 16:17:29 -0800

    created navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,23 +2,30 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import HomePage from './pages/home'
 import StandingsPage from './pages/standings'
+import SchedulePage from './pages/schedule'
+import TeamsPage from './pages/teams'
+import WatchPage from './pages/watch'
+import NewsPage from './pages/news'
 
 import Header from './components/header'
 import Footer from './components/footer'
 
 function App() {
   return (
-    <>
     <Router>
     <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="standings" element={<StandingsPage />} />
+        <Route path="schedule" element={<SchedulePage />} />
+        <Route path="teams" element={<TeamsPage />} />
+        <Route path="watch" element={<WatchPage />} />
+        <Route path="news" element={<NewsPage />} />
+
         <Route path="*" element={<p>404: Not Found</p>} />
       </Routes>
     <Footer />
     </Router>
-    </> 
   )
 }
 
--- Frontend/src/components/header/Navbar.jsx
+++ Frontend/src/components/header/Navbar.jsx
@@ -1,18 +1,42 @@
-import {Link} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 
 export default function Navbar(){
+    const location = useLocation()
 
     return(<>
 
-        <div className="bg-gray-100">
-
-            <p>This is the heading</p>
-
-            <Link to="standings">Standings</Link>
-
+        <div className="bg-gray-900 flex place-content-between pt-4 pb-4 pl-20 pr-20">
+            <div>
+                <NavbarLink linkto="/">Home</NavbarLink>
+                <NavbarLink linkto="/watch">Watch</NavbarLink>
+                <NavbarLink linkto="/news">News</NavbarLink>
+                <NavbarLink linkto="/standings">Standings</NavbarLink>
+                <NavbarLink linkto="/schedule">Schedule</NavbarLink>
+                <NavbarLink linkto="/teams">Teams</NavbarLink>
+            </div>
+            <div>
+                <NavbarLink linkto="/signup">Sign Up</NavbarLink>
+            </div>
         </div>
         
-    
-    
     </>)
+}
+
+function NavbarLink({linkto, children}){
+    const style = 'mr-6 ml-6 text-blue-500 hover:text-blue-400 active:text-blue-300 hover:border-b-3 '+ 
+        (linkto.toLowerCase().split('/')[1] == location.pathname.split('/')[1] ? 'border-b-3' : ' ')
+    return(
+        <Link to={linkto} className={style}>
+            {children}
+        </Link>
+    )
+}
+
+function NavbarDropdown({linkto, children}){
+    const style = 'mr-6 ml-6 text-blue-500 hover:text-blue-400 active:text-blue-300 hover:border-b-3 '
+    return(
+        <>
+        <Link to={linkto} className={style}>{children}</Link>
+        </>
+    )
 }
